e3222bc9065ee83ae58e9976731508c86f1611fd
kjempelodott <kjempelodott@gmail.com>
2015-10-09 15:57:32 +0200

    python3 style relative imports

--- banan/__init__.py
+++ banan/__init__.py
@@ -1,4 +1,5 @@
-from db import TransactionsDB
-from parsers import yAbankCSVParser, yAbankPDFParser, BankNorwegianCSVParser
-from config import Config
-from server import Server
+from .db import TransactionsDB
+from .parsers import yAbankCSVParser, yAbankPDFParser, BankNorwegianCSVParser
+from .config import Config
+from .server import Server
+from .logger import DEBUG, INFO, WARN, ERROR
